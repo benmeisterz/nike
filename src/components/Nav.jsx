79dda3e019d7a856312d7147bf111b9395fd5888
benmeisterz <ben.naldoza@gmail.com>
2023-08-29 11:07:52 +0800

    burger

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
                             height={29} />
                     </a>
                     <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-                        {navLinks.map((item) => (
+                        {navLinks.map(item => (
                             <li key={item.label}>
                                 <a href={item.href}
                                     className='font=montserrat leading-normal text-lg text-slate-gray'
@@ -27,11 +27,12 @@ const Nav = () => {
                         ))}
 
                     </ul>
-                    <div>
-                        <img src="{hamburger}"
+                    <div className='hidden max-lg:block'>
+                        <img src={hamburger}
                             width={25}
                             height={25}
                             alt="Hamburger" />
+
                     </div>
                 </nav>
             </header>
