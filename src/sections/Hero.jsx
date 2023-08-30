a25e35f7edb8b91144552cb4b2e70542d5ee3f5f
benmeisterz <ben.naldoza@gmail.com>
2023-08-30 11:17:53 +0800

    img

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,9 @@
 import Button from '../components/Button'
+import ShoeCard from '../components/ShoeCard'
 import { arrowRight } from '../assets/icons'
-import { statistics } from '../constants'
+import { shoes, statistics } from '../constants'
+import { bigShoe1 } from '../assets/images'
+
 
 const Hero = () => {
     return (
@@ -12,8 +15,7 @@ const Hero = () => {
                     <p className='text-xl font-montserrat text-coral-red'>Our Summer Collection</p>
                     <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'><span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
                         <br />
-                        <span className='text-coral-red inline-block mt-3 mr-5'>Nike </span>
-                        Shoes
+                        <span className='text-coral-red inline-block mt-3'>Nike </span> Shoes
                     </h1>
                     <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:nax-w-sm'>Discover stylish Nike arrivals, quality comfort and innovation for your active life.</p>
                     <Button label="Shop Now"
@@ -29,7 +31,27 @@ const Hero = () => {
                         ))}
                     </div>
                 </div>
-
+                <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+                    <img
+                        src={bigShoe1}
+                        alt="shoe collection"
+                        width={610}
+                        height={500}
+                        className='object-contain relative z-10'
+                    />
+                </div>
+                <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px'>
+                    {shoes.map((shoe) => (
+                        <div key={shoe}>
+                            <ShoeCard
+                                imgURL={shoe}
+                                changeBigShoeImage=
+                                {() => { }}
+                                bigShoeImage=""
+                            />
+                        </div>
+                    ))}
+                </div>
 
             </section>
         </div>
